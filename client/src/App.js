222f7df069f7641ac9bb7258c1cfbc08de6ea5e3
hugo <hugo@pop-os.localdomain>
2020-09-24 14:01:02 +0200

    :lipstick: change CSS to materialize

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,25 @@
 import React, { Fragment, useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import * as JqueryFunctions from './assets/libs/customJquery'
+import M from 'materialize-css'
 import './App.scss'
-import Login from './components/pages/Login'
-import Dashboard from './components/pages/Dashboard'
-import Sidebar from './components/layout/Sidebar'
-import Navbar from './components/layout/Navbar'
-import Footer from './components/layout/Footer'
 
 import AuthState from './context/auth/AuthState'
+import AlertState from './context/alert/AlertState'
 
 const App = () => {
-	//Use animation jquery methods
 	useEffect(() => {
-		JqueryFunctions.toogleSideNavigation()
-		JqueryFunctions.menuAccordionsResized()
-		JqueryFunctions.preventContentWrapper()
-		JqueryFunctions.topButtonAppear()
-		JqueryFunctions.smoothScrollingTop()
-	})
+		M.AutoInit()
+	}, [])
+
 	return (
 		<AuthState>
-			<Router>
-				<Fragment>
-					<Switch>
-						<Route exact path='/' component={Login} />
-						<div id='wrapper'>
-							<Sidebar />
-							<div id='content-wrapper' className='d-flex flex-column'>
-								<div id='content'>
-									<Navbar />
-									<Route exact path='/dashboard' component={Dashboard} />
-								</div>
-								<Footer />
-							</div>
-						</div>
-					</Switch>
-					<a className='scroll-to-top rounded' href='#page-top'>
-						<i className='fas fa-angle-up'></i>
-					</a>
-				</Fragment>
-			</Router>
+			<AlertState>
+				<Router>
+					<Fragment>
+						<Switch></Switch>
+					</Fragment>
+				</Router>
+			</AlertState>
 		</AuthState>
 	)
 }

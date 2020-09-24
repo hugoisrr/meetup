538f89a1e377e1101650ff348f1f605c27bc6580
hugo <hugo@pop-os.localdomain>
2020-09-24 09:00:52 +0200

    :lipstick: bootstrap style added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
-import './App.css';
+import React, {useEffect} from 'react';
+import * as JqueryFunctions from './assets/libs/customJquery'
+import './App.scss'
 
-function App() {
+const App = () => {
+  //Use animation jquery methods
+	useEffect(() => {
+		JqueryFunctions.toogleSideNavigation()
+		JqueryFunctions.menuAccordionsResized()
+		JqueryFunctions.preventContentWrapper()
+		JqueryFunctions.topButtonAppear()
+		JqueryFunctions.smoothScrollingTop()
+	})
   return (
     <div className="App">
       <h1>MeetUp</h1>
